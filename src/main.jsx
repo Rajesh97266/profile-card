de4fa68977c991821de8666a4c54e7678f747a47
Rajesh97266 <rajesh97266@gmail.com>
2024-12-10 14:35:03 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
+import ProfileCard from "./components/ProfileCard";
+
+createRoot(document.getElementById("root")).render(<ProfileCard />);
